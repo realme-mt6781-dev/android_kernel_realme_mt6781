01ec3f54a0c649bd7b19a35302913487094d8b04
Sidath Senanayake <sidaths@google.com>
2021-12-04 17:10:06 +0600

    drivers: mtk-gpu: bifrost: Add multiplier for timeout limits
    Adds TICK_MULTIPLIER which is multiplied into various timeout values.
    This can be set such that slower systems (such as emulation) can be
    allowed to run longer in (wall clock) time before timing out.
    
    Change-Id: I1aaa472d0d7048c6fdcb738986514755d95befd2
    Signed-off-by: Sidath Senanayake <sidaths@google.com>
    Co-authored-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: helliini <113885617+helliini@users.noreply.github.com>

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_config_defaults.h
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_config_defaults.h
@@ -19,6 +19,18 @@
  *
  */
 
+/*
+ * Increase multiplier to increase timeout limit for:
+ *
+ *  - JS_HARD_STOP_TICKS_SS
+ *  - JS_SOFT_JOB_TIMEOUT
+ *  - JS_RESET_TICKS_SS
+ *
+ * Default is 1
+ */
+#define TICK_MULTIPLIER (1)
+
+
 /**
  * DOC: Default values for configuration settings
  *
@@ -134,7 +146,7 @@ enum {
 /**
  * Default minimum number of scheduling ticks before jobs are hard-stopped
  */
-#define DEFAULT_JS_HARD_STOP_TICKS_SS    (50) /* 5s */
+#define DEFAULT_JS_HARD_STOP_TICKS_SS    (50 * TICK_MULTIPLIER) /* Default: 5s */
 
 /**
  * Default minimum number of scheduling ticks before CL jobs are hard-stopped.
@@ -151,13 +163,13 @@ enum {
  * Default timeout for some software jobs, after which the software event wait
  * jobs will be cancelled.
  */
-#define DEFAULT_JS_SOFT_JOB_TIMEOUT (3000) /* 3s */
+#define DEFAULT_JS_SOFT_JOB_TIMEOUT (3000 * TICK_MULTIPLIER) /* Default: 3s */
 
 /**
  * Default minimum number of scheduling ticks before the GPU is reset to clear a
  * "stuck" job
  */
-#define DEFAULT_JS_RESET_TICKS_SS           (55) /* 5.5s */
+#define DEFAULT_JS_RESET_TICKS_SS           (55 * TICK_MULTIPLIER) /* Default: 5.5s */
 
 /**
  * Default minimum number of scheduling ticks before the GPU is reset to clear a
