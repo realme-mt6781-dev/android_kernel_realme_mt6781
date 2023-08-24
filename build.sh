0c424f71a0b6a1d51fe8e0a26436ae12e3255cc4
Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
2023-08-24 20:15:39 +0000

    build_sh: Check if compilers exist already before cloning them * Can get rid of git fatal error repo already exists * switch to neutron clang * Rebrand to InfixKernel-spaced

--- build.sh
+++ build.sh
@@ -2,19 +2,30 @@
 clear
 function compile() 
 {
-echo COSMOS Kernel, CODENAMED - SPACED
+echo InfixKernel-spaced, CODENAMED - spaced
 echo
 sleep 3 >/dev/null
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G >/dev/null
 export ARCH=arm64
-export KBUILD_BUILD_HOST=COSMOS
-export KBUILD_BUILD_USER="INFIX"
-git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
-git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
-git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+export KBUILD_BUILD_HOST=InfixKernel-spaced
+export KBUILD_BUILD_USER="HELLINFIX"
 
+clangbin=clang/bin/clang
+if ! [ -a $clangbin ]; then mkdir clang && cd clang
+sudo apt install -y libelf-dev libarchive-tools zstd flex bc ccache
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) --patch=glibc
+ls
+cd ..
+fi
+gcc64bin=los-4.9-64/bin/aarch64-linux-android-as
+if ! [ -a $gcc64bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+fi
+gcc32bin=los-4.9-32/bin/arm-linux-androideabi-as
+if ! [ -a $gcc32bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+fi
 read -p "Wanna do dirty build? (Y/N): " build_type
 if [[ $build_type == "N" || $build_type == "n" ]]; then
 echo Deleting out directory and doing clean Build
@@ -37,28 +48,41 @@ fi
 make O=out ARCH=arm64 spaced_defconfig
 
 PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC="clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
-                      LD=ld.lld \
-                      AS=llvm-as \
-		              AR=llvm-ar \
-			          NM=llvm-nm \
-			          OBJCOPY=llvm-objcopy \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
+make -j$(nproc --all)   O=out \
+                        ARCH=arm64 \
+                        CC="clang" \
+                        CLANG_TRIPLE=aarch64-linux-gnu- \
+                        CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
+                        CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                        LD=ld.lld \
+                        AS=llvm-as \
+                        AR=llvm-ar \
+                        NM=llvm-nm \
+                        LLVM=1 \
+                        LLVM_IAS=1 \
+                        OBJCOPY=llvm-objcopy \
+                        STRIP=llvm-strip \
+                        CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
 }
 
 function zupload()
 {
-git clone --depth=1 https://github.com/HELLINFIX/AnyKernel3.git AnyKernel
+zimage=out/arch/arm64/boot/Image.gz-dtb
+if ! [ -a $zimage ];
+then
+echo  " Failed to compile zImage, fix the errors first "
+else
+echo -e " Build succesful, generating flashable zip now "
+anykernelbin=AnyKernel/anykernel.sh
+if ! [ -a $anykernelbin ]; then git clone --depth=1 https://github.com/HELLINFIX/AnyKernel3 AnyKernel
+fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 InfixKernel-spaced-1.0-spaced.zip *
+zip -r9 InfixKernel-spaced-OSS-Kernel.zip *
 curl -sL https://git.io/file-transfer | sh
-./transfer fio InfixKernel-spaced-1.0-spaced.zip
+./transfer fio InfixKernel-spaced-OSS-Kernel.zip
+cd ../
+fi
 }
 
 compile
