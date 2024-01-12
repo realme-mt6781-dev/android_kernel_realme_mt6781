3772ae9516a0de7c6c456ca76256439b0029c81b
HELLINFIX <mridulsharma57570@gmail.com>
2024-01-12 09:47:25 +0000

    Revert "mali_kbase: pm: Enhance power policy interface"
    This reverts commit 4bade48072afbfdce8ea0c85906d3dc6abf1347c.

--- drivers/gpu/mediatek/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
+++ drivers/gpu/mediatek/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -2902,8 +2902,7 @@ static ssize_t set_pm_poweroff(struct device *dev,
 
 	stt = &kbdev->pm.backend.shader_tick_timer;
 	stt->configured_interval = HR_TIMER_DELAY_NSEC(gpu_poweroff_time);
-	stt->default_ticks = poweroff_shader_ticks;
-	stt->configured_ticks = stt->default_ticks;
+	stt->configured_ticks = poweroff_shader_ticks;
 
 	spin_unlock_irqrestore(&kbdev->hwaccess_lock, flags);
 
@@ -2941,7 +2940,7 @@ static ssize_t show_pm_poweroff(struct device *dev,
 	stt = &kbdev->pm.backend.shader_tick_timer;
 	ret = scnprintf(buf, PAGE_SIZE, "%llu %u 0\n",
 			ktime_to_ns(stt->configured_interval),
-			stt->default_ticks);
+			stt->configured_ticks);
 
 	spin_unlock_irqrestore(&kbdev->hwaccess_lock, flags);
 
