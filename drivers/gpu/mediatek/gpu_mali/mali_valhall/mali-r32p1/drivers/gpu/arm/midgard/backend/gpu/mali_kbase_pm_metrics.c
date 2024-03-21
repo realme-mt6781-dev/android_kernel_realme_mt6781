d3230ff2bc6eeabc130788e7a5f6fadf3fc0e357
Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
2024-03-21 10:03:27 +0000

    drivers/gpu: Improve DVFS utilization accuracy Get the current time at the same time the utilization registers are accessed, and disable interrupts on RT while it happens so that nothing can interrupt the calculation and hurt the accuracy of the numbers.
    This ensures that inaccurate DVFS utilization numbers aren't fed into the
    governor, which should therefore improve the frequency selections made by
    the governor.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_metrics.c
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_pm_metrics.c
@@ -196,15 +196,7 @@ static void kbase_pm_get_dvfs_utilisation_calc(struct kbase_device *kbdev)
 	 */
 	err = kbase_ipa_control_query(
 		kbdev, kbdev->pm.backend.metrics.ipa_control_client,
-		&gpu_active_counter, 1, &protected_time);
-
-	/* Read the timestamp after reading the GPU_ACTIVE counter value.
-	 * This ensures the time gap between the 2 reads is consistent for
-	 * a meaningful comparison between the increment of GPU_ACTIVE and
-	 * elapsed time. The lock taken inside kbase_ipa_control_query()
-	 * function can cause lot of variation.
-	 */
-	now = ktime_get();
+        &gpu_active_counter, 1, &protected_time, &now);
 
 	if (err) {
 		dev_err(kbdev->dev,
